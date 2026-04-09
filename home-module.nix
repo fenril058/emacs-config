6974a64558bc9e5e5e9795f696bdd293addc586b
ril <fenril.nh@gmail.com>
2026-04-10 03:43:12 +0900

    change home-module settings

--- home-module.nix
+++ home-module.nix
@@ -1,60 +1,74 @@
 {
   config,
+  lib,
   pkgs,
   flake,
   ...
 }:
 let
   system = pkgs.stdenv.hostPlatform.system;
+  inherit (lib) types;
 in
 {
-  programs.emacs-twist = {
-    enable = true;
-    emacsclient.enable = true;
-    createInitFile = true;
-    createManifestFile = true;
-    config = flake.packages.${system}.default;
-    earlyInitFile = flake.earlyInitEl.${system};
-  };
 
-  xdg.desktopEntries.emacs-nix = {
-    name = "Emacs (nix)";
-    genericName = "Text Editor";
-    comment = "GNU Emacs Text Editor";
-    exec = "${config.home.profileDirectory}/bin/emacs %F";
-    icon = "${pkgs.emacs-gtk}/share/icons/hicolor/128x128/apps/emacs.png";
-    terminal = false;
-    startupNotify = true;
-    categories = [
-      "Utility"
-      "Development"
-      "TextEditor"
-    ];
-    mimeType = [
-      "text/plain"
-      "text/x-makefile"
-      "text/x-c"
-      "text/x-c++"
-      "text/x-java"
-      "application/x-shellscript"
-    ];
-    settings = {
-      StartupWMClass = "Emacs";
+  options = {
+    programs.emacs-twist.settings = {
+      extraFeatures = lib.mkOption {
+        type = types.listOf types.str;
+        description = "List of options";
+        default = [ ];
+      };
     };
   };
 
-  home.packages = with pkgs; [
-    just
-    nixd
-    hunspell
-    hunspellDicts.en_US
+  config = lib.mkIf config.programs.emacs-twist.enable {
+    programs.emacs-twist = {
+      emacsclient.enable = true;
+      createInitFile = true;
+      createManifestFile = true;
+      config = flake.packages.${system}.default;
+      earlyInitFile = flake.earlyInitEl.${system};
+    };
 
-    # Packages below are managed in init.org
-    # tree-sitter
-    # nixfmt
-  ];
+    xdg.desktopEntries.emacs-nix = {
+      name = "Emacs (nix)";
+      genericName = "Text Editor";
+      comment = "GNU Emacs Text Editor";
+      exec = "${config.home.profileDirectory}/bin/emacs %F";
+      icon = "${pkgs.emacs-gtk}/share/icons/hicolor/128x128/apps/emacs.png";
+      terminal = false;
+      startupNotify = true;
+      categories = [
+        "Utility"
+        "Development"
+        "TextEditor"
+      ];
+      mimeType = [
+        "text/plain"
+        "text/x-makefile"
+        "text/x-c"
+        "text/x-c++"
+        "text/x-java"
+        "application/x-shellscript"
+      ];
+      settings = {
+        StartupWMClass = "Emacs";
+      };
+    };
 
-  home.file = {
-    ".config/emacs/snippets".source = ./snippets;
+    home.packages = with pkgs; [
+      just
+      nixd
+      hunspell
+      hunspellDicts.en_US
+
+      # Packages below are managed in init.org
+      # tree-sitter
+      # nixfmt
+    ];
+
+    home.file = {
+      ".config/emacs/snippets".source = ./snippets;
+    };
   };
 }
